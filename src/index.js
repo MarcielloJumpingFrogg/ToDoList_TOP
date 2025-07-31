c4aa8d78aa4c462ec9360c0652a3c50658c68458
MainPc <147655500+MarcielloJumpingFrogg@users.noreply.github.com>
2025-08-01 00:06:03 +0200

    stuff

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/* 
+
+{
+                "listItemId" : "",
+                "title" : "",
+                "description" : "",
+                "dueDate" : "",
+                "priority": 0,
+                "checked" : false
+            }
+
+*/
+
+
+//const list = () => import('./itemList.json')
+
+import * as list from './itemList.json'
+
+console.log(list)
